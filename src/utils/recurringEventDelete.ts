d0679249dbdbbe290a7b27c9d0f79797ec74433f
hmyo2853 <hmyo2853@gmail.com>
2025-08-27 18:29:00 +0900

    Refactor : 🔄 반복 일정 단일 삭제 테스트 코드 리팩토링

--- src/utils/recurringEventDelete.ts
+++ src/utils/recurringEventDelete.ts
@@ -1,5 +1,8 @@
 import { Event } from '../types';
 
+const isNotTargetEvent = (eventIdToDelete: string) => (event: Event) =>
+  event.id !== eventIdToDelete;
+
 export function deleteSingleRecurringEvent(events: Event[], eventIdToDelete: string): Event[] {
-  return events.filter((event) => event.id !== eventIdToDelete);
+  return events.filter(isNotTargetEvent(eventIdToDelete));
 }
